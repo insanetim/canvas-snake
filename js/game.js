7ae00681b8681569fde8cccebd86e8693dac142c
insanetim <47120724+insanetim@users.noreply.github.com>
2022-01-18 17:46:22 +0200

    update

--- js/game.js
+++ js/game.js
@@ -3,8 +3,18 @@ const game = {
   ctx: null,
   board: null,
   snake: null,
-  width: 640,
-  height: 360,
+  width: 0,
+  height: 0,
+  dimensions: {
+    max: {
+      width: 640,
+      height: 360,
+    },
+    min: {
+      width: 300,
+      height: 300,
+    },
+  },
   sprites: {
     background: null,
     cell: null,
@@ -17,6 +27,31 @@ const game = {
   init() {
     this.canvas = document.getElementById("mycanvas");
     this.ctx = this.canvas.getContext("2d");
+    this.initDimensions();
+  },
+  initDimensions() {
+    const data = {
+      maxWidth: this.dimensions.max.width,
+      maxHeight: this.dimensions.max.height,
+      minWidth: this.dimensions.min.width,
+      minHeight: this.dimensions.min.height,
+      realWidth: window.innerWidth,
+      realHeight: window.innerHeight,
+    };
+
+    this.fitHeight(data);
+
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+  },
+  fitHeight(data) {
+    this.width = Math.floor(
+      (data.realWidth * data.maxHeight) / data.realHeight
+    );
+    this.width = Math.min(this.width, data.maxWidth);
+    this.width = Math.max(this.width, data.minWidth);
+    this.height = Math.floor((this.width * data.realHeight) / data.realWidth);
+    this.canvas.style.height = "100%";
   },
   preload(callback) {
     let loaded = 0;
@@ -38,7 +73,11 @@ const game = {
     this.snake.create();
 
     window.requestAnimationFrame(() => {
-      this.ctx.drawImage(this.sprites.background, 0, 0);
+      this.ctx.drawImage(
+        this.sprites.background,
+        (this.width - this.sprites.background.width) / 2,
+        (this.height - this.sprites.background.height) / 2
+      );
       this.board.render();
       this.snake.render();
     });
